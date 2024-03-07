a8150f9c9d9d1243d57812d981425720aa23be6b
anish-2903 <bowled.by.boult@gmail.com>
2024-03-08 01:03:49 +0530

    Count

--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.firstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // This code will be executed when the Button is clicked
+                showToast("Button Clicked!");
+                updateCount();
+
+            }
+        });
     }
+
+    private void updateCount() {
+        count++;
+        textView.setText(String.valueOf(count));
+    }
+    // Helper method to show a Toast message
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
